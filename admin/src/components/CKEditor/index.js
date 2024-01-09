afbd6297d032170379ea1b09614d5a376aafbd0b
Alejandro Abaroa <alejandro.abaroa@platform.sh>
2024-01-09 12:11:26 -0800

    Edit styling to fix strapi overriding all margins in editor body

--- admin/src/components/CKEditor/index.js
+++ admin/src/components/CKEditor/index.js
@@ -14,19 +14,144 @@ const Wrapper = styled.div`
     /* Fixes https://github.com/strapi/strapi/issues/11849 */
     /* Fix line height */
     .ck-editor__editable {
-      line-height: initial;
+      /* line-height: initial; */
+      line-height: 1.75;
     }
     /* Revert font styling */
-    div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
-      font: revert;
-    }
+    /* div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video { */
+      /* font: revert; */
+    /* } */
     /* Revert list styling */
     ul.todo-list {
       list-style: none;
     }
     ul, ol {
       list-style: initial;
-      margin-left: 32px;
+    }
+
+    li {
+      margin-left: 24px;
+      margin-bottom: 16px;
+    }
+
+    p {
+      font-size: 16px;
+      line-height: 28px;
+      margin-bottom: 24px;
+
+      @media(min-width: 768px) {
+        font-size: 18px;
+        line-height: 32px;
+        margin-bottom: 32px;
+      }
+    }
+
+    h1 {
+      font-size: 30px;
+      line-height: 40px;
+      margin-bottom: 24px;
+
+      @media(min-width: 768px) {
+        font-size: 36px;
+        line-height: 48px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 60px;
+        line-height: 84px;
+        margin-bottom: 32px;
+      }
+    }
+
+    h1 {
+      font-size: 30px;
+      line-height: 40px;
+      margin-bottom: 24px;
+
+      @media(min-width: 768px) {
+        font-size: 36px;
+        line-height: 48px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 60px;
+        line-height: 84px;
+        margin-bottom: 32px;
+      }
+    }
+
+    h2 {
+      font-size: 24px;
+      line-height: 36px;
+      margin-bottom: 16px;
+
+      @media(min-width: 768px) {
+        font-size: 30px;
+        line-height: 40px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 48px;
+        line-height: 64px;
+        margin-bottom: 24px;
+      }
+    }
+
+    h3 {
+      font-size: 20px;
+      line-height: 32px;
+      margin-bottom: 16px;
+
+      @media(min-width: 768px) {
+        font-size: 24px;
+        line-height: 36px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 36px;
+        line-height: 48px;
+        margin-bottom: 24px;
+      }
+    }
+
+    h4 {
+      font-size: 18px;
+      line-height: 28px;
+      margin-bottom: 8px;
+
+      @media(min-width: 768px) {
+        font-size: 20px;
+        line-height: 32px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 24px;
+        line-height: 36px;
+        margin-bottom: 16px;
+      }
+    }
+
+    h5 {
+      font-size: 16px;
+      line-height: 24px;
+      margin-bottom: 8px;
+
+      @media(min-width: 768px) {
+        font-size: 18px;
+        line-height: 28px;
+      }
+
+      @media(min-width: 992px) {
+        font-size: 20px;
+        line-height: 32px;
+        margin-bottom: 16px;
+      }
+    }
+
+    h6 {
+      font-size: 24px;
+      line-height: 36px;
+      margin-bottom: 36;
     }
   }
 `;
